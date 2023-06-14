8e8a3b3834b54361b597650f521118e330b5c537
Phan Ngọc My <64948822+mymyPM@users.noreply.github.com>
2023-06-14 11:30:22 +0700

    Update linear.py
    change more

--- linear.py
+++ linear.py
@@ -17,7 +17,7 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, encoding='latin-1')
     df.to_csv("data.csv", index = False)
 
-X = df.drop(columns=['STV'])
+X = df.drop(columns=['giatri'])
 y = df['STV']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state= 12)
